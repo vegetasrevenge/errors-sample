c109314130fa63b5ce9bedfe4b7d590c89ef593b
vegetasrevenge <cjedwards233@yahoo.com>
2021-08-25 09:53:07 -0500

    init commit

---
+++ src/main/java/com/codeup/errorssample/ErrorsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.errorssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorsSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/errorssample/ErrorsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.errorssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
